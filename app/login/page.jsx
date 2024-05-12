9ed40552f679bc8cb885ba05a6c15d50c94198ec
faysal <devfaysalbd@gmail.com>
2024-05-12 19:37:40 +0600

    some update

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,15 +1,14 @@
-import LoginForm from '@/components/auth/LoginForm';
 import Link from 'next/link';
-import React from 'react';
+import LoginForm from '@/components/auth/LoginForm';
 
 const LoginPage = () => {
     return (
         <section className="h-screen grid place-items-center">
-            <div className="max-w-[650px] w-full mx-auto p-6 border border-gray-700/20 rounded-md">
+            <div className="max-w-[450px] w-full mx-auto p-6 border border-gray-700/20 rounded-md">
                 <h4 className="font-bold text-2xl">Sign in</h4>
                 <LoginForm />
                 <span className="text-center text-xs text-gray-500">
-                    Don't have an account?
+                    Dont have an account?
                     <Link
                         className="underline hover:text-indigo-600 mx-1"
                         href="/register"
